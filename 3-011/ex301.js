4e27cb10161ed7ac4603f4901798ef87bfe2a3ff
Myoungjun Kim <info@apptable.net>
2013-03-21 01:27:37 +0900

    enchant.js template

---
+++ 3-011/ex301.js
@@ -0,0 +1,17 @@
+// enchant.js 템플릿용
+// Copyright (c)2013 Apptable.net by MJ
+enchant();
+
+window.onload = function() {
+	var game = new Game();
+	
+	game.onload = function() {
+		var scene = game.rootScene;
+		scene.backgroundColor = "blue";
+		
+		// 여기에 코딩을 합니다!
+		
+	};
+	
+	game.start();
+};
