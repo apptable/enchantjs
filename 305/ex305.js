4782fa3ad4a265d1ca5b2bf9640c54536f7a63b2
Myoungjun Kim <info@apptable.net>
2013-03-27 00:19:00 +0900

    12장 예제5

---
+++ 305/ex305.js
@@ -0,0 +1,24 @@
+// enchant.js 엔티티를 만들어 Scene에 추가
+// Copyright (c)2013 Apptable.net by MJ
+enchant();
+
+window.onload = function() {
+	var game = new Game(640, 1135); // 게임화면 너비와 높이를 설정
+	game.fps = 16;
+	game.onload = function() {
+		var scene = game.rootScene;	
+		// 여기에 코딩을 합니다!
+		// 라벨 생성
+		var lbl = new Label();
+		lbl.moveTo(45, 45);
+		lbl.text = "시간은 금이다!";
+		lbl.color = "blue";
+		lbl.font = "28px '맑은 고딕', 'Verdana'";
+		scene.addChild(lbl); // 씬에 추가함
+		lbl.onenterframe = function() {
+			this.opacity = (Math.cos(game.frame*4*Math.PI/180)+1)*0.5;
+		};
+	};
+
+	game.start();
+};
